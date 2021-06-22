027e980d6361f1f6518369a5366b9781f948f1a9
qingqingdaren <1142828423@qq.com>
2021-06-23 02:31:48 +0800

    Revert "舆情分析与社会计算作业"
    This reverts commit 200bf341416a2383e8d67bdf99dd165646139a22.

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,12 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hard to guess string'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://zhaoqing:123456@rm-2zeevewh0670zu318.mysql.rds.aliyuncs.com:3306/cucnews'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://zhaoqing:123456@localhost:3306/cucnews'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:12345678@127.0.0.1:3306/cucnews'
     app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 
     db.init_app(app)
-    from .manwei import news as news_blueprint
+    from .news import news as news_blueprint
     app.register_blueprint(news_blueprint)
 
     return app
